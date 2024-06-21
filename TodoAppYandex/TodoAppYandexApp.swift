a003b19fe673c21541819f9d8f182f66c950975f
gregorygromov <gregorygromow@icloud.com>
2024-06-22 02:00:45 +0300

    Initial Commit

---
+++ TodoAppYandex/TodoAppYandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppYandexApp.swift
+//  TodoAppYandex
+//
+//  Created by Григорий Громов on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppYandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

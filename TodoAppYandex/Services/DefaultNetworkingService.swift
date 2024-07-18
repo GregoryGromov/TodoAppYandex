f669bdc8adeb0f7282968a764dbde4da664a751d
gregorygromov <gregorygromow@icloud.com>
2024-07-18 23:16:29 +0300

    Made probably final code refactoring in DefaultNetworkingService

--- TodoAppYandex/Services/DefaultNetworkingService.swift
+++ TodoAppYandex/Services/DefaultNetworkingService.swift
@@ -17,7 +17,8 @@ class DefaultNetworkingService {
     private let testDeveiceID = "iphoneXSgregory"
     private let token = "Amras"
     
-//    #1 "Получить список с сервера"
+//  MARK: - #1 "Получить список с сервера"
+    
     func getList() async throws -> (list: [TodoItem], revision: Int) {
         let url = try makeURL(forMode: .getAll)
         let request = try makeURLRequest(forMode: .getAll, url: url)
@@ -34,7 +35,8 @@ class DefaultNetworkingService {
         throw NetworkError.unknownError
     }
 
-//    #2 "Обновить список на сервере"
+//  MARK: - #2 "Обновить список на сервере"
+    
     func updateList(with list: [TodoItem], revision: Int) async throws -> (list: [TodoItem], revision: Int) {
         
         let url = try makeURL(forMode: .patch)
@@ -52,7 +54,8 @@ class DefaultNetworkingService {
         throw NetworkError.unknownError
     }
 
-//    #3 "Получить элемент списка"
+//  MARK: - #3 "Получить элемент списка"
+    
     func getElement(byId id: String) async throws -> (item: TodoItem, revision: Int) {
         let url = try makeURL(forMode: .getItem, elementId: id)
         let request = try makeURLRequest(forMode: .getItem, url: url)
@@ -69,7 +72,8 @@ class DefaultNetworkingService {
         throw NetworkError.unknownError
     }
 
-//    #4 "Добавить элемент списка"
+//  MARK: - #4 "Добавить элемент списка"
+    
     func addElement(_ todoItem: TodoItem, revision: Int) async throws -> (item: TodoItem, revision: Int) {
         let url = try makeURL(forMode: .post)
         let request = try makeURLRequest(forMode: .post, url: url, revision: revision, element: todoItem)
@@ -86,9 +90,9 @@ class DefaultNetworkingService {
         throw NetworkError.unknownError
     }
 
-//    #5 "Изменить элемент списка"
+//  MARK: - #5 "Изменить элемент списка"
+    
     func updateElement(byId id: String, with todoItem: TodoItem, revision: Int) async throws -> (item: TodoItem, revision: Int) {
-//        print("Функция запущена")
         let url = try makeURL(forMode: .put, elementId: id)
         let request = try makeURLRequest(forMode: .put, url: url, revision: revision, element: todoItem)
 
@@ -105,7 +109,8 @@ class DefaultNetworkingService {
     }
     
     
-//    #6 "Удалить элемент списка"
+//  MARK: - #6 "Удалить элемент списка"
+    
     func deleteElement(byId id: String, revision: Int) async throws -> (item: TodoItem, revision: Int) {
         let url = try makeURL(forMode: .delete, elementId: id)
         let request = try makeURLRequest(forMode: .delete, url: url, revision: revision)
@@ -121,6 +126,8 @@ class DefaultNetworkingService {
         
         throw NetworkError.unknownError
     }
+    
+//  MARK: - Handling response status
     
     private func isRequestSuccessful(response: HTTPURLResponse) -> Bool {
         switch response.statusCode {
@@ -146,6 +153,8 @@ class DefaultNetworkingService {
             throw NetworkError.unknownError
         }
     }
+    
+//  MARK: - Handling response
     
     private func handleServerResponse(data: Data, response: URLResponse, mode: RequestMode) throws -> Any {
         if let httpResponse = response as? HTTPURLResponse {
@@ -186,7 +195,6 @@ class DefaultNetworkingService {
            let dictionary = responseData as? [String: Any],
            let element = dictionary[NetworkingKeys.element] as? [String: Any],
            let revision = dictionary[NetworkingKeys.revision] as? Int{
-//            print("Dict answer:", dictionary)
             if let todoItem = try TodoItem.parseNetworking(json: element) {
                 return (todoItem, revision)
             }
@@ -194,6 +202,8 @@ class DefaultNetworkingService {
         throw DataStorageError.JSONSerializingFailed
     }
     
+//  MARK: - Request creation
+
     private func makeURL(forMode mode: RequestMode, elementId: String? = nil) throws -> URL {
         switch mode {
         case .getAll, .patch, .post:
@@ -303,279 +313,3 @@ class DefaultNetworkingService {
         }
     }
 }
-
-
-
-
-
-//
-////    PATCH обновить спиок #2 - OK
-////    удаляет имеющийся и добавляет новый
-//    func updateList(with list: [TodoItem], revision: Int) async throws -> [TodoItem] {
-//
-//        guard let url = URL(string: baseURL + "/list") else {
-//            throw NetworkError.URLCreationFailed
-//        }
-//
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = "PATCH"
-//        request.setValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-//        request.setValue(String(revision), forHTTPHeaderField: "X-Last-Known-Revision")
-//        request.httpBody = try createHttpBody(list: list)
-//
-//        do {
-//            let (data, response) = try await URLSession.shared.dataTask(for: request)
-//            if let httpResponse = response as? HTTPURLResponse {
-//                switch httpResponse.statusCode {
-//                case 200...299:
-//                    if let responseData = try? JSONSerialization.jsonObject(with: data, options: []),
-//                       let dictionary = responseData as? [String: Any],
-//                       let list = dictionary[NetworkingKeys.list] as? [[String: Any]] {
-//                        var todoItems = [TodoItem]()
-//                        for element in list {
-//                            if let todoItem = try TodoItem.parseNetworking(json: element) {
-//                                todoItems.append(todoItem)
-//                            }
-//                        }
-//                        return todoItems
-//                    }
-//                case 400:
-//                    throw NetworkError.incorrectRequestFormat
-//                case 401:
-//                    throw NetworkError.incorrectAuthorization
-//                case 404:
-//                    throw NetworkError.elementNotFound
-//                case 500:
-//                    throw NetworkError.serverError
-//                default:
-//                    throw NetworkError.unknownError
-//                }
-//            }
-//        } catch {
-//            throw error
-//        }
-//        
-//        throw NetworkError.unknownError
-//    }
-//
-////    GET element #3 - OK
-//    func getElement(byId id: String) async throws -> TodoItem {
-//        guard let url = URL(string: baseURL + "/list/" + id) else {
-//            throw NetworkError.URLCreationFailed
-//        }
-//
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = "GET"
-//        request.setValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-//
-//        do {
-//            let (data, response) = try await URLSession.shared.dataTask(for: request)
-//            if let httpResponse = response as? HTTPURLResponse {
-//                switch httpResponse.statusCode {
-//                case 200...299:
-//                    print("Success: \(httpResponse.statusCode)")
-//                    if let responseData = try? JSONSerialization.jsonObject(with: data, options: []),
-//                       let dictionary = responseData as? [String: Any],
-//                       let element = dictionary[NetworkingKeys.element] as? [String: Any] {
-//                        if let todoItem = try TodoItem.parseNetworking(json: element) {
-//                            return todoItem
-//                        }
-//                    }
-//                case 400:
-//                    throw NetworkError.incorrectRequestFormat
-//                case 401:
-//                    throw NetworkError.incorrectAuthorization
-//                case 404:
-//                    throw NetworkError.elementNotFound
-//                case 500:
-//                    throw NetworkError.serverError
-//                default:
-//                    throw NetworkError.unknownError
-//                }
-//            }
-//        } catch {
-//            throw error
-//        }
-//        
-//        throw NetworkError.unknownError
-//    }
-//
-////    POST #4 - ОК
-//    func addElement(_ todoItem: TodoItem, revision: Int) async throws -> TodoItem {
-//        guard let url = URL(string: baseURL + "/list") else {
-//            throw NetworkError.URLCreationFailed
-//        }
-//
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = "POST"
-//        request.setValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-//        request.setValue(String(revision), forHTTPHeaderField: "X-Last-Known-Revision")
-//        request.httpBody = try createHttpBody(element: todoItem)
-//
-//        do {
-//            let (data, response) = try await URLSession.shared.dataTask(for: request)
-//            if let httpResponse = response as? HTTPURLResponse {
-//                switch httpResponse.statusCode {
-//                case 200...299:
-//                    if let responseData = try? JSONSerialization.jsonObject(with: data, options: []),
-//                       let dictionary = responseData as? [String: Any],
-//                       let element = dictionary[NetworkingKeys.element] as? [String: Any] {
-//                        if let todoItem = try TodoItem.parseNetworking(json: element) {
-//                            return todoItem
-//                        }
-//                    }
-//                case 400:
-//                    throw NetworkError.incorrectRequestFormat
-//                case 401:
-//                    throw NetworkError.incorrectAuthorization
-//                case 404:
-//                    throw NetworkError.elementNotFound
-//                case 500:
-//                    throw NetworkError.serverError
-//                default:
-//                    throw NetworkError.unknownError
-//                }
-//            }
-//        } catch {
-//            throw error
-//        }
-//        
-//        throw NetworkError.unknownError
-//    }
-//
-//    //    PUT #5
-//    func updateElement(byId id: String, with todoItem: TodoItem, revision: Int) async throws -> TodoItem {
-//
-//        guard let url = URL(string: baseURL + "/list/" + id) else {
-//            throw NetworkError.URLCreationFailed
-//        }
-//
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = "PUT"
-//        request.setValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-//        request.setValue(String(revision), forHTTPHeaderField: "X-Last-Known-Revision")
-//        request.httpBody = try createHttpBody(element: todoItem)
-//
-//        do {
-//            let (data, response) = try await URLSession.shared.dataTask(for: request)
-//            if let httpResponse = response as? HTTPURLResponse {
-//                switch httpResponse.statusCode {
-//                case 200...299:
-//                    print("Success: \(httpResponse.statusCode)")
-//                    if let responseData = try? JSONSerialization.jsonObject(with: data, options: []),
-//                       let dictionary = responseData as? [String: Any],
-//                       let element = dictionary[NetworkingKeys.element] as? [String: Any] {
-//                        if let todoItem = try TodoItem.parseNetworking(json: element) {
-//                            return todoItem
-//                        }
-//                    }
-//                case 400:
-//                    throw NetworkError.incorrectRequestFormat
-//                case 401:
-//                    throw NetworkError.incorrectAuthorization
-//                case 404:
-//                    throw NetworkError.elementNotFound
-//                case 500:
-//                    throw NetworkError.serverError
-//                default:
-//                    throw NetworkError.unknownError
-//                }
-//            }
-//        } catch {
-//            throw error
-//        }
-//        
-//        throw NetworkError.unknownError
-//    }
-//
-//// DELETE #6 - OK
-//    func deleteElement(byId id: String, revision: Int) async throws -> TodoItem {
-//        guard let url = URL(string: baseURL + "/list/" + id) else {
-//            throw NetworkError.URLCreationFailed
-//        }
-//
-//        print("URL:", baseURL + "/list/" + id)
-//
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = "DELETE"
-//        request.setValue("Bearer " + token, forHTTPHeaderField: "Authorization")
-//        request.setValue(String(revision), forHTTPHeaderField: "X-Last-Known-Revision")
-//
-//        do {
-//            let (data, response) = try await URLSession.shared.dataTask(for: request)
-//            if let httpResponse = response as? HTTPURLResponse {
-//                switch httpResponse.statusCode {
-//                case 200...299:
-//                    print("Success: \(httpResponse.statusCode)")
-//                    if let responseData = try? JSONSerialization.jsonObject(with: data, options: []),
-//                       let dictionary = responseData as? [String: Any],
-//                       let element = dictionary[NetworkingKeys.element] as? [String: Any] {
-//                        if let todoItem = try TodoItem.parseNetworking(json: element) {
-//                            return todoItem
-//                        }
-//                    }
-//                case 400:
-//                    throw NetworkError.incorrectRequestFormat
-//                case 401:
-//                    throw NetworkError.incorrectAuthorization
-//                case 404:
-//                    throw NetworkError.elementNotFound
-//                case 500:
-//                    throw NetworkError.serverError
-//                default:
-//                    throw NetworkError.unknownError
-//                }
-//            }
-//        } catch {
-//            throw error
-//        }
-//        
-//        throw NetworkError.unknownError
-//    }
-//
-//    private func createHttpBody(list: [TodoItem]) throws -> Data {
-//        var listOfElement = [[String: Any]]()
-//
-//        for element in list {
-//            guard var decodedElement = element.jsonNetworking as? [String: Any] else {
-//                throw DataStorageError.convertingDataFailed
-//            }
-//            decodedElement[JSONKeys.lastUpdatedBy] = testDeveiceID
-//            listOfElement.append(decodedElement)
-//        }
-//
-//        var httpBodyDict = [String: Any]()
-//        httpBodyDict[NetworkingKeys.status] = "ok"
-//        httpBodyDict[NetworkingKeys.list] = listOfElement
-//
-//        do {
-//            let httpBody = try JSONSerialization.data(withJSONObject: httpBodyDict, options: [])
-//            return httpBody
-//        } catch {
-//            throw DataStorageError.JSONSerializingFailed
-//        }
-//    }
-//
-//    private func createHttpBody(element: TodoItem) throws -> Data {
-//        guard var decodedElement = element.jsonNetworking as? [String: Any] else {
-//            throw DataStorageError.convertingDataFailed
-//        }
-//        decodedElement[JSONKeys.lastUpdatedBy] = testDeveiceID
-//
-//        var httpBodyDict = [String: Any]()
-//        httpBodyDict[NetworkingKeys.status] = "ok"
-//        httpBodyDict[NetworkingKeys.element] = decodedElement
-//
-//        do {
-//            let httpBody = try JSONSerialization.data(withJSONObject: httpBodyDict, options: [])
-//            return httpBody
-//        } catch {
-//            throw DataStorageError.JSONSerializingFailed
-//        }
-//    }
-//}
